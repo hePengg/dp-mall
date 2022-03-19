b7474bb52b1882987035b8e4b4e1bce572a92d10
hepeng <peng6.he@tcl.com>
2022-03-20 01:57:57 +0800

    init project

---
+++ mall-goods/src/main/java/com/dp/mall/goods/GoodsBootstrap.java
@@ -0,0 +1,16 @@
+package com.dp.mall.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HePeng
+ * @date 2022/3/20 上午1:35
+ */
+@SpringBootApplication
+public class GoodsBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsBootstrap.class, args);
+    }
+}
